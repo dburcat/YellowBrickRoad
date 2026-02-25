1f9bd7aa924fe2a308e6f1cff6893adfcff1eaf1
david <dburcad@gmail.com>
2026-02-25 09:44:35 -0500

    Add Hello Python work, finished

---
+++ NanoEditor/hello.py
@@ -0,0 +1,7 @@
+def main():
+	print("Hello, Python!")
+	print("Hello, again!")
+
+if __name__ == "__main__":
+	main()
+
---
+++ NanoEditor/yowza.py
@@ -0,0 +1,7 @@
+print("Yowza!")
+i = 42
+print(i)
+d = 2.71828
+print(d)
+s = "Zipcode Rocks!"
+print(s)
